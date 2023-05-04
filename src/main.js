4815ab9e85d907791b68f3aa4c0125aed2697bfb
Sergey Romanov <sergeytomanovv94@gmail.com>
2023-05-05 00:58:52 +0300

    The project was initialized and the main skeleton of the project was made, a telegram bot was connected, a service for recording and converting an audio file

---
+++ src/main.js
@@ -0,0 +1,32 @@
+import config from "config";
+import { Telegraf } from "telegraf";
+import { message } from "telegraf/filters";
+import { ogg } from "./service/ogg.js";
+
+const bot = new Telegraf(config.get("TELEGRAM_TOKEN"));
+
+bot.on(message("text"), async (ctx) => {
+  await ctx.reply(JSON.stringify(ctx.message, null, 2));
+});
+
+bot.on(message("voice"), async (ctx) => {
+  try {
+    const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
+    const userId = String(ctx.message.from.id);
+    console.log("userId", userId);
+    const oggPath = await ogg.create(link.href, userId);
+    const mp3Path = await ogg.toMp3(oggPath, userId);
+    await ctx.reply(mp3Path, userId);
+  } catch (error) {
+    console.log("ERROR while voice message", error.message);
+  }
+});
+
+bot.command("start", async (ctx) => {
+  await ctx.reply(JSON.stringify(ctx.message, null, 2));
+});
+
+bot.launch();
+
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
---
+++ src/utils/removeFile.js
@@ -0,0 +1,1 @@
+export async function removeFile(path) {}
