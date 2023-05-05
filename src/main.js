ca33a1807f5c42c54dc0bedbed2222e3509a19b1
Sergey Romanov <sergeytomanovv94@gmail.com>
2023-05-06 02:14:50 +0300

    openai service done

--- src/main.js
+++ src/main.js
@@ -1,31 +1,75 @@
 import config from "config";
-import { Telegraf } from "telegraf";
+import { Telegraf, session } from "telegraf";
 import { message } from "telegraf/filters";
+import { code } from "telegraf/format";
 import { ogg } from "./service/ogg.js";
 import { openai } from "./service/openai.js";
 
+const INITIAL_SESSION = {
+  messages: [],
+};
+
 const bot = new Telegraf(config.get("TELEGRAM_TOKEN"));
 
+bot.use(session());
+
+bot.command("new", async (ctx) => {
+  ctx.session = INITIAL_SESSION;
+  await ctx.reply("Жду вашего запроса");
+});
+
+bot.command("start", async (ctx) => {
+  ctx.session = INITIAL_SESSION;
+  await ctx.reply("Жду вашего запроса");
+});
+
 bot.on(message("text"), async (ctx) => {
-  await ctx.reply(JSON.stringify(ctx.message, null, 2));
+  ctx.session ??= INITIAL_SESSION;
+  try {
+    await ctx.reply("Обрабатываю...");
+
+    ctx.session.messages.push({
+      role: openai.roles.USER,
+      content: ctx.message.text,
+    });
+    const response = await openai.chat(ctx.session.messages);
+
+    ctx.session.messages.push({
+      role: openai.roles.ASSISTANT,
+      content: response.content,
+    });
+
+    await ctx.reply(response.content);
+  } catch (error) {
+    console.log("ERROR while text message", error.message);
+    await ctx.reply("Ошибка сервера");
+  }
 });
 
 bot.on(message("voice"), async (ctx) => {
+  ctx.session ??= INITIAL_SESSION;
   try {
+    await ctx.reply(code("Обрабатываю..."));
     const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
     const userId = String(ctx.message.from.id);
     const oggPath = await ogg.create(link.href, userId);
     const mp3Path = await ogg.toMp3(oggPath, userId);
     const text = await openai.transcription(mp3Path);
-    // const response = await openai.chat(text);
-    await ctx.reply(text);
+    await ctx.reply(`Ваш запрос: ${text}`);
+
+    ctx.session.messages.push({ role: openai.roles.USER, content: text });
+    const response = await openai.chat(ctx.session.messages);
+
+    ctx.session.messages.push({
+      role: openai.roles.ASSISTANT,
+      content: response.content,
+    });
+
+    await ctx.reply(response.content);
   } catch (error) {
     console.log("ERROR while voice message", error.message);
+    await ctx.reply("Ошибка сервера");
   }
-});
-
-bot.command("start", async (ctx) => {
-  await ctx.reply(JSON.stringify(ctx.message, null, 2));
 });
 
 bot.launch();
--- src/service/openai.js
+++ src/service/openai.js
@@ -3,6 +3,11 @@ import { createReadStream } from "fs";
 import { Configuration, OpenAIApi } from "openai";
 
 class OpenAI {
+  roles = {
+    ASSISTANT: "assistant",
+    USER: "user",
+    SYSTEM: "system",
+  };
   constructor(apiKey) {
     const configuration = new Configuration({
       apiKey,
@@ -10,7 +15,17 @@ class OpenAI {
     this.openai = new OpenAIApi(configuration);
   }
 
-  chat() {}
+  async chat(messages) {
+    try {
+      const response = await this.openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages,
+      });
+      return response.data.choices[0].message;
+    } catch (error) {
+      console.log("ERROR while gpt chat", error.message);
+    }
+  }
 
   async transcription(filePath) {
     try {
